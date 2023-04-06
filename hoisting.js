669a10ca0e8661a8ab7e6cde995b9d3490eaba4d
siimongc <siimongaviria30@gmail.com>
2023-04-06 16:07:33 -0500

    lo que habia en el pc de mesa

---
+++ hoisting.js
@@ -0,0 +1,26 @@
+/*Es llamar variables en un orden en especifico es decir usarlas despues de declararlas no antes ya que saldria undefine, por que primero
+se debe declarar antes de usar la variable */
+
+/*cuando en visula studio code aparece la palabra como opaca es por que la variable o funcion no se esta usando*/
+
+//Bien
+
+var nombre = "Aaron";
+
+function saludo() {
+    console.log("Hola " + nombre);
+}
+
+saludo();
+
+
+
+//mal
+
+saludo();
+
+function saludo() {
+    console.log("Hola " + nombre);
+}
+
+var nombre = "Aaron";
